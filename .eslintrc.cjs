7fd59ef43465ede02c9a3382ebb46a259a47991c
ChoJinmok <jjmok1127@gmail.com>
2023-05-08 14:28:05 +0900

    chore: eslint 추가

---
+++ .eslintrc.cjs
@@ -0,0 +1,98 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  parser: '@typescript-eslint/parser',
+  plugins: [
+    'react',
+    '@typescript-eslint',
+    '@react-native'
+  ],
+  extends: [
+    'airbnb',
+    'airbnb-typescript',
+    'airbnb/hooks',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:promise/recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  rules: {
+    indent: ['error', 2],
+    'no-trailing-spaces': 'error',
+    curly: 'error',
+    'brace-style': 'error',
+    'no-multi-spaces': 'error',
+    'space-infix-ops': 'error',
+    'space-unary-ops': 'error',
+    'no-whitespace-before-property': 'error',
+    'func-call-spacing': 'error',
+    'space-before-blocks': 'error',
+    'keyword-spacing': ['error', { before: true, after: true }],
+    'comma-spacing': ['error', { before: false, after: true }],
+    'comma-style': ['error', 'last'],
+    'comma-dangle': ['error', 'always-multiline'],
+    'space-in-parens': ['error', 'never'],
+    'block-spacing': 'error',
+    'array-bracket-spacing': ['error', 'never'],
+    'object-curly-spacing': ['error', 'always'],
+    'key-spacing': ['error', { mode: 'strict' }],
+    'arrow-spacing': ['error', { before: true, after: true }],
+    'jsx-a11y/label-has-associated-control': ['error', { assert: 'either' }],
+    'linebreak-style': 'off',
+    'no-proto': 'off',
+
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx', 'ts', '.tsx'] }],
+    'react/jsx-props-no-spreading': ['error', {
+      exceptions: ['Component'],
+    }],
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
+
+    'react/style-prop-object': [2, {
+      allow: ['StatusBar']
+    }]
+  },
+  overrides: [
+    {
+      files: ['*.js', '*.ts', '*.tsx', '*.d.ts'],
+      rules: {
+        '@typescript-eslint/no-var-requires': 'off',
+      },
+      parserOptions: {
+        project: './tsconfig.json',
+      }
+    },
+  ],
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx', '.js'],
+    },
+    'import/resolver': {
+      typescript: './tsconfig.json',
+      node: {
+        extensions: ['.js', '.jsx', '.json', '.native.js']
+      }
+    },
+  },
+  ignorePatterns: ['*.cjs'],
+};
