0aa17e1eab9317473ca613fcd12afd1733ea33d1
ChoJinmok <jjmok1127@gmail.com>
2023-05-21 00:28:59 +0900

    2.6 Styles part two

--- App.tsx
+++ App.tsx
@@ -1,17 +1,39 @@
 import React from 'react';
-import {SafeAreaView, StyleSheet, View, Text} from 'react-native';
+import {
+  SafeAreaView,
+  StyleSheet,
+  View,
+  Text,
+  ScrollView,
+  Dimensions,
+} from 'react-native';
+
+const {width: SCREEN_WIDTH} = Dimensions.get('window');
 
 const App = (): React.JSX.Element => (
   <SafeAreaView style={styles.container}>
     <View style={styles.city}>
       <Text style={styles.cityName}>Seoul</Text>
     </View>
-    <View style={styles.weather}>
-      <View style={styles.day}>
-        <Text style={styles.temperature}>27</Text>
-        <Text style={styles.description}>Sunny</Text>
-      </View>
-    </View>
+
+    <ScrollView
+      pagingEnabled
+      horizontal
+      showsHorizontalScrollIndicator={false}
+
+      // ScrollView의 스타일은 contentContainerStyle로 지정
+      // contentContainerStyle={styles.weather}
+
+      // indicatorStyle은 iOS에서만 동작
+      // indicatorStyle="white"
+    >
+      {Array.from({length: 10}).map((_, index) => (
+        <View key={index} style={styles.day}>
+          <Text style={styles.temperature}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+      ))}
+    </ScrollView>
   </SafeAreaView>
 );
 
@@ -29,13 +51,12 @@ const styles = StyleSheet.create({
     fontSize: 68,
     fontWeight: '500',
   },
-  weather: {
-    flex: 3,
-  },
+  weather: {},
   day: {
     flex: 1,
     alignItems: 'center',
     paddingTop: 50,
+    width: SCREEN_WIDTH,
   },
   temperature: {
     fontSize: 178,
